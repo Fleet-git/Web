f6ff91ae8acdd4f2f5104264af9572e739151d24
root <gvelayutham@fleetstudio.com>
2019-06-27 16:22:09 +0000

    Nde

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var cors = require('cors');
 var app = express();
 app.use(cors());
 
-app.use(express.static(__dirname + '/build'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/*', function(req, res){
-res.status(200).sendFile(__dirname+'/build/index.html');
+res.status(200).sendFile(__dirname+'/public/index.html');
 });
 
 app.listen(80, function() {
